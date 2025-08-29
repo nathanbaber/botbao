262c30af0c0ce3474f638b101a7b42c802631a5e
nathanbaber <nathanbaber0@gmail.com>
2025-08-30 00:31:24 +0300

    edit time2

--- botbao.py
+++ botbao.py
@@ -514,10 +514,12 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Функция бронирование
 async def start_reservation(update: Update, context) -> int:
+    user_id = update.effective_user.id 
     if update.callback_query: # Если вызвано из Inline кнопки
             await update.callback_query.answer()
             message_to_send = update.callback_query.message
             await context.bot.send_message(
+            chat_id=user_id,
             reply_markup=ReplyKeyboardRemove()
         )
     else: # Если вызвано из команды
@@ -537,6 +539,7 @@ async def start_reservation(update: Update, context) -> int:
     ).build()
 
     await context.bot.send_message(
+        chat_id=user_id,
         text="В какой день Вы планируете посетить наше бистро? Пожалуйста, выберите дату:",
         reply_markup=calendar
     )
